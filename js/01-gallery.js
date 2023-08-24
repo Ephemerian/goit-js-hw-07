719c09f9e110ddb15be4cee2ce04a6016b7a3782
Ephemerian <106543055+Ephemerian@users.noreply.github.com>
2023-08-24 03:47:45 +0300

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const listGalleryEl = document.querySelector(".gallery");
+listGalleryEl.addEventListener("click", onClickAtPicture);
+
+setGalleryToListHTML(`beforeend`, createGalleryMarkup(galleryItems));
+
+function onClickAtPicture(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  showBigImg(e.target);
+}
+function showBigImg(target) {
+  const instance = basicLightbox.create(
+    `<img src="${target.dataset.source}" width="800" height="600">`
+  );
+  instance.show();
+
+  listGalleryEl.addEventListener("keydown", (evt) => {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+  });
+}
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(
+      ({ description, preview, original }) =>
+        `<li class="gallery__item">
+        <a class="gallery__link" href="large-image.jpg">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>`
+    )
+    .join("");
+}
+
+function setGalleryToListHTML(place, gallery) {
+  listGalleryEl.insertAdjacentHTML(place, gallery);
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryListEl = document.querySelector(".gallery");
+
+gallerySetToHtml(createGalleryMarkup2(galleryItems));
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
+
+function createGalleryMarkup2(galleryItems) {
+  return galleryItems
+    .map(
+      ({ description, preview, original }) =>
+        `<li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+             <img class="gallery__image" src="${preview}" alt="${description}" />
+          </a>
+       </li>`
+    )
+    .join("");
+}
+
+function gallerySetToHtml(gallery) {
+  galleryListEl.innerHTML = gallery;
+}
